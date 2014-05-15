97240fecf081703b3000efb87a6b27cbfaaad29b
Robin_Weipeng_Chan <280296084@qq.com>
2014-05-15 18:45:46 +0800

    validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
-	has_many :microposts
+    validates(:name, presence: true,length:{maximum:50} )
+
+    VALID_EAMIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates(:email, presence:true,format:{with:VALID_EAMIL_REGEX},uniqueness:true )
+    
+
+
+
 end
